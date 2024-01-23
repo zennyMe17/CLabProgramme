97cc4907357907c4fb1bc979cfad0f52343f1b14
zennyMe17 <gowda.hemanth.1718@gmail.com>
2024-01-23 17:13:48 +0530

    Little Bit of Change

--- lab1.c
+++ lab1.c
@@ -2,9 +2,9 @@
 #include<stdlib.h>
 
 void create();
-void display();
-void insert();
-void delete();
+//void display();
+//void insert();
+//void delete();
 
 int arr[50],n;
 
@@ -17,17 +17,17 @@ int main(){
         printf("Enter 3 for Inserting an element in an Array :\n");
         printf("Enter 4 for Deleting an element in an Array :\n");
         printf("Enter 5 for exit\n");
-        scanf("%d",&choice)
-        switch (choice)
+        scanf("%d",&choice);
+        switch(choice)
         {
         case 1 : create();
         break;
-        case 2 : display();
-        break;
-        case 3 : insert();
-        break;
-        case 4 : delete();
-        break;
+        //case 2 : display();
+        //break;
+        //case 3 : insert();
+        //break;
+        //case 4 : delete();
+        //break;
         case 5 : exit(0);
         break;
 
@@ -44,6 +44,6 @@ void create(){
     printf("Enter the numbers = ");
     for (int i=0;i++;i<n)
     {
-        scanf("%d",&a[i])
+        scanf("%d",&arr[i]);
     }
 }
