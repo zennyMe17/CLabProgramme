eccd5bc78f76f5b064a92f598d8cbbd60ca39519
Hemanth S <gowda.hemanth.1718@gmail.com>
2024-02-15 11:41:12 +0530

    Update Programme 11 DoubleLinkedList.c

--- Programme 11 DoubleLinkedList.c
+++ Programme 11 DoubleLinkedList.c
@@ -2,37 +2,40 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-struct node{
+struct node
+{
     int data;
-    struct node *prev, *next;
+    struct node *next;
+    struct node *prev;
 };
 
-struct node *head=0, *newnode, *temp;
+struct node *head=0, *tail, *newnode, *temp;
 
-void create()
+void insert()
 {
-    newnode=(struct node *)malloc(sizeof(struct node));
-    printf("Enter the element = ");
+    newnode = (struct node *)malloc(sizeof(struct node));
+    printf("Enter the data = ");
     scanf("%d",&newnode->data);
-    newnode->prev=0;
     newnode->next=0;
+    newnode->prev=0;
+    
     if (head == 0)
     {
-        head=temp=newnode;
-    }
-    else
-    {
-        newnode->prev=temp;
-        temp->next=newnode;
-        temp=newnode;
+        head=tail=newnode;
     }
     
+    else
+    {
+        tail->next=newnode;
+        newnode->prev=tail;
+        tail=newnode;
+    }
 }
 
 void display()
 {
     temp=head;
-    while (temp != 0)
+    while(temp != 0)
     {
         printf("%d\t",temp->data);
         temp=temp->next;
@@ -40,29 +43,63 @@ void display()
     printf("\n");
 }
 
+void delete()
+{
+    int val;
+    temp=head;
+    printf("Enter the value to be deleted = ");
+    scanf("%d",&val);
+    while (temp != 0)
+    {
+        if (temp->data == val)
+        {
+            if (temp == head)
+            {
+                head=temp->next;
+                head->prev=0;
+                free(temp);
+            }
+            else
+            {
+                temp->prev->next=temp->next;
+                temp->next->prev=temp->prev;
+                free(temp);
+            }
+            return;
+        }
+        temp=temp->next;
+    }
+}
 void main()
 {
     int ch;
-    while(1){
-    printf("Enter the choice\n1. To create\n2. To display\n3. To exit\n");
-    scanf("%d",&ch);
-    switch (ch)
-    {
-        case 1:
+    while(1)
+    {   
+        printf("Enter\n1. To insert a node\n2. To display\n3. To delete at entered value\n4. To exit\n");
+        scanf("%d",&ch);
+        switch(ch)
         {
-            create();
-            break;
+            case 1: 
+            {
+                insert();
+                break;
+            }
+            case 2: 
+            {
+                display();
+                break;
+            }
+            case 3: 
+            {
+                delete();
+                break;
+            }
+            case 4: 
+            {
+                exit(0);
+                break;
+            }
+            
         }
-        case 2:
-        {
-            display();
-            break;
-        }
-        case 3:
-        {
-            exit(0);
-            break;
-        }
-    }
     }
 }
